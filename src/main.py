cf178efa503c36b634408dec2f735308cffda77a
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-11-10 17:54:18 -0700

    Working with Gray Scale

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ def main():
 
     cap = cv2.VideoCapture(0)
 
+    face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
 
 
     while True:
@@ -14,11 +18,13 @@ def main():
 
         ret, frame = cap.read()
 
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
 
 
         # Display the resulting frame
 
-        cv2.imshow('Webcam', frame)
+        cv2.imshow('Laptop Cam', gray_image)
 
 
 
